e75eb495e59130986ab2cad1758b588e96fd1f42
fateh moonesi <f.moonesi@jsk.local>
2024-10-23 14:53:43 +0330

    main image changed

--- src/components/animated-text.tsx
+++ src/components/animated-text.tsx
@@ -127,7 +127,7 @@ export default function AnimatedText({
         </div>
       </div>
 
-      <p className="text-sm md:text-lg mt-2 lg:mt-4 text-justify text-muted-foreground dark:text-white">
+      <p className="text-sm md:text-[16px] leading-6 mt-2 lg:mt-4 text-justify text-muted-foreground dark:text-white">
         لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
         از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و
         سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای
--- src/components/layout/first-section.tsx
+++ src/components/layout/first-section.tsx
@@ -2,13 +2,16 @@ import AnimatedText from "../animated-text";
 import { MainImage } from "../main-image";
 export function FirstSection() {
   return (
-    <div className="py-12 px-4 lg:px-8 flex justify-center">
-      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 items-center md:max-w-[1122px] gap-3 lg:gap-8">
-        <div className="order-last lg:order-first">
-          <AnimatedText />
-        </div>
-        <div className="order-first lg:order-last">
-          <MainImage />
+    <div className="flex justify-center">
+      <div className="relative -mt-[5.75rem] overflow-hidden pt-12 md:pt-[5.75rem] md:max-w-[1300px]">
+        <div className="absolute inset-y-0 hidden w-full min-w-[1360px]  lg:block"></div>
+        <div className="mx-auto max-w-container px-4 pt-4 sm:px-6 lg:flex lg:px-8">
+          <div className="relative z-20 mx-auto max-w-[40rem] pb-16 pt-16 lg:mx-0 lg:w-[40rem] lg:max-w-none lg:flex-none lg:pb-24 lg:pr-4 lg:pt-20">
+            <AnimatedText />
+          </div>
+          <div className="relative z-10 mt-12 hidden select-none lg:flex">
+            <MainImage />
+          </div>
         </div>
       </div>
     </div>
